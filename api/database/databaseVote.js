d9a9623707eef32b947ac4393c15d1401d9c446f
papiricoh <slendeman0200@gmail.com>
2024-11-20 05:39:20 +0100

    formatted dbVote

--- api/database/databaseVote.js
+++ api/database/databaseVote.js
@@ -7,10 +7,10 @@ const db = {
         const connection = await pool.getConnection();
         try {
             const [rows] = await connection.query(`SELECT government_members.id, government_members.role, users.first_name, users.last_name, users.username, parties.label, parties.name
- FROM government_members
- INNER JOIN users ON government_members.user_id = users.id
- INNER JOIN users_parties ON government_members.user_id = users_parties.user_id
- INNER JOIN parties ON users_parties.party_id = parties.id`);
+            FROM government_members
+            INNER JOIN users ON government_members.user_id = users.id
+            INNER JOIN users_parties ON government_members.user_id = users_parties.user_id
+            INNER JOIN parties ON users_parties.party_id = parties.id`);
             return rows;
         }catch (err) {
             throw new Error("DB error: " + err);
