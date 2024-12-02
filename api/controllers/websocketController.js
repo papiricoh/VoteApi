a7fbaea30f71582977d3300eaa70e06180ef7735
papiricoh <slendeman0200@gmail.com>
2024-12-02 22:48:39 +0100

    enhance websocket controller; add client management and ping-pong functionality

--- api/controllers/websocketController.js
+++ api/controllers/websocketController.js
@@ -1,23 +1,41 @@
 //const db = require('../database/databaseGame');
 
+const clients = new Set();
 
 exports.mainWS = async (ws, req) => {
 
-    ws.on('connection', async () => {
-      console.log('Cliente conectado');
-    });
-  
-    ws.on('message', async (msg) => {
-      console.log(`Mensaje recibido: ${msg}`);
+  console.log('Cliente conectado');
+  clients.add(ws);
+  ws.send(JSON.stringify('Bienvenido al servidor WebSocket'));
 
-        ws.send(JSON.stringify(msg))
-    });
-  
-    ws.on('close', () => {
-      console.log('Cliente desconectado');
-    });
-  
-    ws.on('error', (err) => {
-      console.error('Error en la conexión WebSocket:', err);
-    });
+  ws.on('message', async (msg) => {
+    msg = JSON.parse(msg);
+    console.log(`Mensaje recibido: ${msg}`);
+    if(msg.type === 'ping') {
+      ws.send(JSON.stringify('pong'));
+      return;
+    }
+
+    ws.send(JSON.stringify(msg))
+  });
+
+  ws.on('close', () => {
+    console.log('Cliente desconectado');
+    clients.delete(ws);
+  });
+
+  ws.on('error', (err) => {
+    console.error('Error en la conexión WebSocket:', err);
+  });
+}
+
+const broadcast = (message) => {
+  for (const client of clients) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
   }
+};
+
+exports.clients = clients;
+exports.broadcast = broadcast;
